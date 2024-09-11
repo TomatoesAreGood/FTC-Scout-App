9165c952eb725ccd949860c022ccfec396d91d8d
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2024-09-11 09:45:21 -0400

    ListView Drawer

--- lib/pages/createAccount.dart
+++ lib/pages/createAccount.dart
@@ -11,7 +11,6 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
-
   Widget generateTitle(String title){
     return Text(
       title,
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -17,92 +17,95 @@ class _MyDrawerState extends State<MyDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: [
-          const SizedBox(
-            width: double.maxFinite,
-            child: DrawerHeader(
-              decoration: BoxDecoration(color: Colors.black,),
-              child: Image(image:AssetImage('assets/logo.png')),
+      child: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Column(
+          children: [
+            const SizedBox(
+              width: double.maxFinite,
+              child: DrawerHeader(
+                decoration: BoxDecoration(color: Colors.black,),
+                child: Image(image:AssetImage('assets/logo.png')),
+              ),
             ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text("Settings"),
-            onTap: (){
-             Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => 
-                    const Settings()
-                )
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.question_mark),
-            title: const Text("About Us"),
-             onTap: (){
-             Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => 
-                    const About()
-                )
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.privacy_tip_outlined),
-            title: const Text("Privacy Policy"),
-             onTap: (){
-             Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => 
-                    const PrivacyPolicy()
-                )
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.warning_amber),
-            title: const Text("Report a Problem"),
-             onTap: (){
-             Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => 
-                    const ReportProblem()
-                )
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text("Sign Out"),
-            onTap: (){
-              showDialog(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                  contentPadding: EdgeInsets.zero,
-                  title: const Text("Are you sure you want to sign out?", textAlign: TextAlign.center, style: TextStyle(fontSize: 15)),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        UserPreferences.logOut();
-                        setState(() {
-                          MyApp.reload(context);
-                        });
-                      },
-                      child: const Text('Yes'),
-                    ),
-                    TextButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text('Cancel'),
-                    ),
-                  ],
-                )
-              );
-            },
-          ),
-        ],
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text("Settings"),
+              onTap: (){
+               Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => 
+                      const Settings()
+                  )
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.question_mark),
+              title: const Text("About Us"),
+               onTap: (){
+               Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => 
+                      const About()
+                  )
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.privacy_tip_outlined),
+              title: const Text("Privacy Policy"),
+               onTap: (){
+               Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => 
+                      const PrivacyPolicy()
+                  )
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.warning_amber),
+              title: const Text("Report a Problem"),
+               onTap: (){
+               Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => 
+                      const ReportProblem()
+                  )
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text("Sign Out"),
+              onTap: (){
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    contentPadding: EdgeInsets.zero,
+                    title: const Text("Are you sure you want to sign out?", textAlign: TextAlign.center, style: TextStyle(fontSize: 15)),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          UserPreferences.logOut();
+                          setState(() {
+                            MyApp.reload(context);
+                          });
+                        },
+                        child: const Text('Yes'),
+                      ),
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text('Cancel'),
+                      ),
+                    ],
+                  )
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
